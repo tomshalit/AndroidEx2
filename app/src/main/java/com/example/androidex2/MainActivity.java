116569a91222883bd12ec91c96a2f07a114abd6d
MorTelTsur <morteltsur@gmail.com>
2019-05-14 12:30:28 +0300

    Main Activity - Fragment connection Beggining

--- app/src/main/java/com/example/androidex2/MainActivity.java
+++ app/src/main/java/com/example/androidex2/MainActivity.java
@@ -6,26 +6,35 @@ import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
+    private String[][]  questions = new String[5][10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        buildQuestions(questions);
         //Passing the data from activity to fragment
-        //Bundle bundleQuestion = new Bundle();
+        Bundle bundleAnswers = new Bundle();
 
-        //bundleQuestion.putString( "q1(key)", "1(value)" );
-        //bundleQuestion.putString( "q2(key correct answer)", "2(value)" );
-        //bundleQuestion.putString( "q3(key)", "3(value)" );
-        //bundleQuestion.putString( "q4(key)", "4(value)" );
+        bundleAnswers.putString( "q1(key)", questions[1][0] );
+        bundleAnswers.putString( "q2(key correct answer)", questions[2][0] );
+        bundleAnswers.putString( "q3(key)", questions[3][0] );
+        bundleAnswers.putString( "q4(key)", questions[4][0] );
 
         // set Fragment1 Arguments
-        //Fragment1 myObj = new Fragment1();
-        //myObj.setArguments( bundleQuestion );
+        Fragment1 myObj = new Fragment1();
+        myObj.setArguments(bundleAnswers);
 
 
-        //getSupportFragmentManager().beginTransaction().replace( R.id.con, Fragment1.newInstance( bundleQuestion ) ).commit();
+        getSupportFragmentManager().beginTransaction().replace( R.id.con, Fragment1.newInstance(bundleAnswers) ).commit();
+    }
+
+    private void buildQuestions(String[][] questions) {
+        questions[0][0] = "In which episode of season 1 did Ned Stark die?";
+        questions[1][0] = "8";
+        questions[2][0] = "9";
+        questions[3][0] = "13";
+        questions[4][0] = "2";
     }
 
 }
